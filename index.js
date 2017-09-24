47764401a59ebf07b127c73ce772193356b96afb
Anuwat Keawson <anuwatk@lanna.co.th>
2017-09-24 23:57:34 +0700

    v1.10

--- index.js
+++ index.js
@@ -20,14 +20,34 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/webhook', line.middleware(config), (req, res) => {
-  res.json(req.body.events)
+//app.post('/webhook', line.middleware(config), (req, res) => {
+  //res.json(req.body.events)
   //client.replyMessage(req.body.events.message.text);
-  client.replyMessage('1234556');
+  //client.replyMessage('1234556');
   //Promise
     //.all(req.body.events.map(handleEvent))
     //.then((result) => res.json(result));
-});
+//});
+
+app.post('/webhook', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
+})
 
 // event handler
 function handleEvent(event) {
@@ -44,7 +64,7 @@ function handleEvent(event) {
 }
 
 // listen on port
-const port = 8080 || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
